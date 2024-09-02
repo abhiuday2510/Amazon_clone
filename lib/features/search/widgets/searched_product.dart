2095f4af49bcab9182fa02943126eed83652078c
abhiuday2510 <abhiuday.ojha@gmail.com>
2024-09-02 06:20:32 +0530

    implements rating on search

--- lib/features/search/widgets/searched_product.dart
+++ lib/features/search/widgets/searched_product.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/common/widgets/stars.dart';
 import 'package:flutter/material.dart';
 
 import '../../../models/product.dart';
@@ -18,10 +19,45 @@ class SearchedProduct extends StatelessWidget {
             children: [
               Image.network(
                 product.images[0],
-                fit: BoxFit.fitHeight,
+                fit: BoxFit.fitWidth,
                 height: 135,
                 width: 135,
               ),
+              Column(
+                children: [
+                Container(
+                  width: 235,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(product.name, style: const TextStyle(fontSize: 16),
+                  maxLines: 2,
+                  ),
+                ),
+                Container(
+                  width: 235,
+                  padding: const EdgeInsets.only(left: 10, top :5),
+                  child: const Stars(rating: 4,)
+                ),
+                Container(
+                  width: 235,
+                  padding: const EdgeInsets.only(left: 10, top :5),
+                  child: Text('\$${product.price}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  maxLines: 2,
+                  ),
+                ),
+                Container(
+                  width: 235,
+                  padding: const EdgeInsets.only(left: 10,),
+                  child: const Text("Eligible for FREE Shipping"
+                  ),
+                ),
+                Container(
+                  width: 235,
+                  padding: const EdgeInsets.only(left: 10, top :5),
+                  child: const Text('In Stock', style: TextStyle(color: Colors.teal),
+                  maxLines: 2,
+                  ),
+                ),
+              ],),
             ],
           ),
         ),
